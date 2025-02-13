1c66165b25f4e9ab76945620e5efb977218a2ce2
Vincent Nguyen <vincentn1102@gmail.com>
2025-02-13 11:04:46 +1030

    s

---
+++ game.py
@@ -0,0 +1,6 @@
+from card import Card
+
+#suits and ranks
+suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+
