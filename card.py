1737aaa266808c7524b073c987fe1e73e7668455
Vincent Nguyen <vincentn1102@gmail.com>
2025-02-21 18:38:17 +1030

    card.py

--- card.py
+++ card.py
@@ -4,7 +4,10 @@ class Card:
     def __init__(self, suit, rank, image_path):
         self.suit = suit
         self.rank = rank
-        self.image = pygame.image.load(image_path)
-    
+        original_image = pygame.image.load(image_path)
+        
+        scaled_width, scaled_height = 80, 120
+        self.image = pygame.transform.scale(original_image, (scaled_width, scaled_height))
+        
     def draw(self, screen, pos):
         screen.blit(self.image, pos)
