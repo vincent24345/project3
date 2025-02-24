3f06e5ab56b52c9c8032e8f1edefef6edbcf2b1e
Vincent Nguyen <vincentn1102@gmail.com>
2025-02-24 16:48:32 +1030

    ai.f

--- ai.py
+++ ai.py
@@ -1,3 +1,10 @@
 from rules import can_beat
 
-def_card_to_play(bot_player, lastcard):
+def choose_card_to_play(bot_player, last_card):
+    #sort the bot's hand 
+    bot_player.hand.sort(key=lambda c: c.rank)
+
+    for i, card in enumerate(bot_player.hand):
+        if can_beat(card, last_card):
+            return i  
+    return None  
